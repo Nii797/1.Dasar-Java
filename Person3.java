84f577e1ce0369d3faf1f870f94285f4f3d67cb5
Nii797 <Phanterasonpardus797@gmail.com>
2021-06-05 22:19:36 +0700

    update pbo menambahkan null pada middleName

--- Person3.java
+++ Person3.java
@@ -4,15 +4,19 @@ public class Person3 {
     // dan hitung dimulai dari nol
     // private
     public static int Count = 0;
-    public String name;
+    public String firstName;
+    // middle untuk overload constuctor
+    public String middleName;
+    public String lastName;
     public int age;
     public double weight;
     public double height;
     public String hobi;
 
     // construct atau constructor
-    Person3(String name, int age, double weight, double height, String hobi) {
-        this.name = name;
+    Person3(String firstName, String lastName, int age, double weight, double height, String hobi) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.age = age;
         this.weight = weight;
         this.height = height;
@@ -21,12 +25,45 @@ public class Person3 {
         Person3.Count++;
     }
 
+    // Person3(String firstName, String middleName, String lastName, int age, double
+    // weight, double height, String hobi) {
+    // this.firstName = firstName;
+    // this.lastName = lastName;
+    // this.age = age;
+    // this.weight = weight;
+    // this.height = height;
+    // this.hobi = hobi;
+    // // menambahkan satu persatu atau sama dengan 1 + 1
+    // Person3.Count++;
+    // this.middleName = middleName;
+    // }
+
+    // cara kedua memanggil constructor sebelumnya
+    Person3(String firstName, String middleName, String lastName, int age, double weight, double height, String hobi) {
+        this(firstName, lastName, age, weight, height, hobi);
+        this.middleName = middleName;
+    }
+
+    // method fullname
+    // public String fullName() {
+    // return this.firstName + " " + this.lastName;
+    // }
+
+    // method fullname versi 1
+    public String fullName() {
+        if (this.middleName == null) {
+            return this.firstName + " " + this.lastName;
+        } else {
+            return this.firstName + " " + this.middleName + " " + lastName;
+        }
+    }
+
     // method
     public void printData() {
 
         System.out.println("");
         System.out.println("===================================================");
-        System.out.println("Nama Saya " + this.name + ".");
+        System.out.println("Nama Saya " + fullName() + ".");
         System.out.println("Umur Saya " + this.age + ".");
 
         if (this.age < 18) {
--- Tampil.java
+++ Tampil.java
@@ -9,12 +9,13 @@ public class Tampil {
         // System.out.println("Masukan");
         // System.out.println("Awal total " + Person3.Count + " Orang.");
 
-        Person3 orang1 = new Person3("Soni", 23, 180.5, 75.7, "ngoding");
-        Person3 orang2 = new Person3("Alex", 24, 170.1, 67.9, "ngegame");
-        Person3 orang3 = new Person3("jhonny", 27, 173.2, 70.7, "musik");
-
+        Person3 orang1 = new Person3("Soni", "Hidayatuloh", 23, 180.5, 75.7, "ngoding");
         orang1.printData();
+
+        Person3 orang2 = new Person3("Alex", "Jhon", "Baret", 24, 170.1, 67.9, "ngegame");
         orang2.printData();
+
+        Person3 orang3 = new Person3("jhonny", "Vincent", 27, 173.2, 70.7, "musik");
         orang3.printData();
 
         Person3.printCount();
