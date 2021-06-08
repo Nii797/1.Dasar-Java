6c3ae67d34c0e510d0b9b5863567f5be2afea43c
Nii797 <Phanterasonpardus797@gmail.com>
2021-06-08 17:03:12 +0700

    update car fuel tank

--- Test/Mobil.java
+++ Test/Mobil.java
@@ -4,6 +4,7 @@ public class Mobil {
     private String nama;
     private String warna;
     private int jarak = 0;
+    private int fuel = 100;
 
     Mobil(String nama, String warna) {
         this.nama = nama;
@@ -14,12 +15,21 @@ public class Mobil {
         System.out.println("Nama Mobil: " + this.nama + ".");
         System.out.println("Warna Mobil: " + this.warna + ".");
         System.out.println("Jarak Mobil: " + this.jarak + " Km.");
+        System.out.println("Bahan Bakar: " + this.fuel + "L");
     }
 
     public void runMobil(int jarak) {
         System.out.println("Bergerak " + jarak + " Km ...");
-        this.jarak += jarak;
+        if (jarak <= this.fuel) {
+            this.jarak += jarak;
+            this.fuel -= jarak;
+        } else {
+            System.out.println("Bahan bakar tidak cukup");
+        }
+
+        // this.jarak += jarak;
         System.out.println("Jarak : " + this.jarak + " Km.");
+        System.out.println("Bahan Bakar: " + fuel + "L");
     }
 
 }
